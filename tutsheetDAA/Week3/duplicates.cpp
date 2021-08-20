ad3b724697fbe45a0b5e377aa3e66efd7d28e388
SURBHI GUSAIN <73434579+SURBHIGUSAIN@users.noreply.github.com>
2021-08-20 12:38:36 +0530

    Add files via upload

---
+++ tutsheetDAA/Week3/duplicates.cpp
@@ -0,0 +1,20 @@
+//using heap sort
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int array[n];
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        cin>>array[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        int maxidx=i;
+        for(j=i+1)
+
+    }
+}
